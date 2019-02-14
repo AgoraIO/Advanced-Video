92a4a981ce19e3041458655095dafd0ed71d36a6
GongYuhua <gongyuhua@gmail.com>
2019-02-14 21:44:09 +0800

    remove unnecessary files

--- MediaPlayer/Mediaplayer-windows/images/simplest_ffmpeg_mem_handler-master/simplest_ffmpeg_mem_handler-master/simplest_ffmpeg_mem_player/compile_gcc.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-gcc simplest_ffmpeg_mem_player.cpp -g -o simplest_ffmpeg_mem_player.out -lstdc++ \
--I /usr/local/include -L /usr/local/lib -lSDLmain -lSDL -lavformat -lavcodec -lavutil -lswscale
-
--- MediaPlayer/Mediaplayer-windows/images/simplest_ffmpeg_mem_handler-master/simplest_ffmpeg_mem_handler-master/simplest_ffmpeg_mem_player/compile_gcc_mac.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-gcc simplest_ffmpeg_mem_player.cpp -g -o simplest_ffmpeg_mem_player.out -lstdc++ \
--framework Cocoa -I /usr/local/include -L /usr/local/lib -lSDLmain -lSDL -lavformat -lavcodec -lavutil -lswscale
-
