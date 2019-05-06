cbbfbdb9a671a53fb28801714191ddbc58b77676
GongYuhua <gongyuhua@gmail.com>
2019-05-06 14:21:40 +0800

    update to rtm

--- Video-Call-with-Chat/OpenDuo-iOS-Objective-C/OpenDuo/LoginViewController.h
+++ Video-Call-with-Chat/OpenDuo-iOS-Objective-C/OpenDuo/LoginViewController.h
@@ -10,6 +10,4 @@
 
 @interface LoginViewController : UIViewController
 
-
 @end
-
--- Video-Call-with-Chat/OpenDuo-iOS-Objective-C/OpenDuo/LoginViewController.m
+++ Video-Call-with-Chat/OpenDuo-iOS-Objective-C/OpenDuo/LoginViewController.m
@@ -10,72 +10,45 @@
 #import "DialViewController.h"
 #import "AlertUtil.h"
 #import "KeyCenter.h"
-#import <AgoraSigKit/AgoraSigKit.h>
+#import <AgoraRtmKit/AgoraRtmKit.h>
 
 @interface LoginViewController ()
-{
-    AgoraAPI *signalEngine;
-}
-
 @property (weak, nonatomic) IBOutlet UITextField *accountTextField;
-
+@property (strong, nonatomic) AgoraRtmKit *signalEngine;
 @end
 
 @implementation LoginViewController
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    
-    signalEngine = [AgoraAPI getInstanceWithoutMedia:[KeyCenter appId]];
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-- (void)viewDidAppear:(BOOL)animated {
-    [super viewDidAppear:animated];
-    
-    __weak typeof(self) weakSelf = self;
-    
-    signalEngine.onLoginSuccess = ^(uint32_t uid, int fd) {
-        NSLog(@"Login successfully, uid: %u", uid);
-        dispatch_async(dispatch_get_main_queue(), ^{
-            [weakSelf performSegueWithIdentifier:@"ShowDialView" sender:@(uid)];
-        });
-    };
-    
-    signalEngine.onLoginFailed = ^(AgoraEcode ecode) {
-        NSLog(@"Login failed, error: %lu", (unsigned long)ecode);
-        dispatch_async(dispatch_get_main_queue(), ^{
-            [AlertUtil showAlert:@"Login failed"];
-        });
-    };
-}
-
-- (void)viewWillDisappear:(BOOL)animated {
-    [super viewWillDisappear:animated];
-    signalEngine.onLoginSuccess = nil;
-    signalEngine.onLoginFailed = nil;
+- (AgoraRtmKit *)signalEngine {
+    if (!_signalEngine) {
+        _signalEngine = [[AgoraRtmKit alloc] initWithAppId:[KeyCenter appId] delegate:nil];
+    }
+    return _signalEngine;
 }
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     if ([segue.identifier isEqualToString:@"ShowDialView"]) {
         DialViewController *dialVC = segue.destinationViewController;
-        dialVC.localUID = [sender unsignedIntValue];
         dialVC.localAccount = self.accountTextField.text;
+        dialVC.signalEngine = self.signalEngine;
     }
 }
 
 - (IBAction)loginButtonClicked:(id)sender {
     NSString *account = self.accountTextField.text;
-    if (account.length > 0) {
-        [self.accountTextField resignFirstResponder];
-        
-        NSString *sigToken = @"_no_need_token";
-        [signalEngine login:[KeyCenter appId] account:account token:sigToken uid:0 deviceID:nil];
+    if (!account.length) {
+        return;
     }
+    
+    [self.accountTextField resignFirstResponder];
+    
+    __weak typeof(self) weakSelf = self;
+    [self.signalEngine loginByToken:nil user:account completion:^(AgoraRtmLoginErrorCode errorCode) {
+        NSLog(@"Login completion code: %ld", (long)errorCode);
+        if (errorCode == AgoraRtmLoginErrorOk) {
+            [weakSelf performSegueWithIdentifier:@"ShowDialView" sender:nil];
+        } else {
+            [AlertUtil showAlert:@"Login failed"];
+        }
+    }];
 }
-
 @end
