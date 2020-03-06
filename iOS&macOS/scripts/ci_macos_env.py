636a93ad571226efc7657a0c247ee16d17742f50
CavanSu <403029552@qq.com>
2020-03-06 15:17:14 +0800

    update ci

---
+++ iOS&macOS/scripts/ci_macos_env.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import re
+import os
+
+def main():
+    
+    # KeyCenter.swift
+    f = open("./shared/Swift/KeyCenter.swift", 'r+')
+    content = f.read()
+    appString = "\"" + appId + "\""
+    tokenString = "\"" + token + "\""
+    contentNew = re.sub(r'<#Your App Id#>', appString, content)
+    contentNew = re.sub(r'<#Temp Access Token#>', tokenString, contentNew)
+    f.seek(0)
+    f.write(contentNew)
+    f.truncate()
+    
+    # KeyCenter.m
+    f = open("./shared/Objective-C/KeyCenter.m", 'r+')
+    content = f.read()
+    appString = "@\"" + appId + "\""
+    tokenString = "@\"" + token + "\""
+    contentNew = re.sub(r'<#Your App Id#>', appString, content)
+    contentNew = re.sub(r'<#Temp Access Token#>', tokenString, contentNew)
+    f.seek(0)
+    f.write(contentNew)
+    f.truncate()
+
+if __name__ == "__main__":
+    main()
